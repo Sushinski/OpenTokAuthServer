121719a96dee86473ebbdc38d99367e7a95149e3
sushinski <pavel.sushi@gmail.com>
2017-07-02 20:46:43 +0300

    adds old keys re-issuing

--- OpenTokAuthServer/views.py
+++ OpenTokAuthServer/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from OpenTokAuthServer.models import Connection, SessionKeys
-from opentok import OpenTok, MediaModes
+# from opentok import OpenTok, MediaModes
 
 TEST_API_KEY = r'45900062'
 TEST_API_SECRET = r'd4c973383d8f9eca272a89e31be614dbd07e60c3'
@@ -12,31 +12,39 @@ TEST_API_SECRET = r'd4c973383d8f9eca272a89e31be614dbd07e60c3'
 def session_new(request, app_key):
     if request.method == 'GET':
         responce = dict()
-        # try to get pending client`s keys
-        conn_obj = Connection.objects.filter(connections__lt=Connection.ALLOWED_CONNECTIONS).last()
-        if conn_obj:
-            responce['apiKey'] = conn_obj.session_key.api_key
-            responce['sessionId'] = conn_obj.session_key.session_id
-            responce['token'] = conn_obj.session_key.token
-            if uuid.UUID(app_key) != conn_obj.opener_app_key:
+        old_conn_obj = Connection.objects.\
+            filter(opener_app_key=uuid.UUID(app_key)).last()
+        if old_conn_obj:
+            responce['apiKey'] = old_conn_obj.session_key.api_key
+            responce['sessionId'] = old_conn_obj.session_key.session_id
+            responce['token'] = old_conn_obj.session_key.token
+            if old_conn_obj.connections == Connection.ALLOWED_CONNECTIONS:
+                old_conn_obj.delete()
+        else:
+            # try to get pending client`s keys
+            conn_obj = Connection.objects.filter(connections__lt=Connection.ALLOWED_CONNECTIONS).last()
+            if conn_obj:
+                responce['apiKey'] = conn_obj.session_key.api_key
+                responce['sessionId'] = conn_obj.session_key.session_id
+                responce['token'] = conn_obj.session_key.token
                 conn_obj.connections += 1
-            conn_obj.save()
-        else:  # to get: new keys
-            opentok_ses = OpenTok(TEST_API_KEY, TEST_API_SECRET)
-            session = opentok_ses.create_session(media_mode=MediaModes.relayed)
-            responce['apiKey'] = TEST_API_KEY
-            responce['sessionId'] = session.session_id
-            responce['token'] = session.generate_token()
-            new_ses_keys = SessionKeys()
-            new_ses_keys.api_key = responce['apiKey']
-            new_ses_keys.session_id = responce['sessionId']
-            new_ses_keys.token = responce['token']
-            new_ses_keys.save()
-            new_con = Connection()
-            new_con.session_key = new_ses_keys
-            new_con.connections = 1
-            new_con.opener_app_key = app_key
-            new_con.save()
+                conn_obj.save()
+            else:  # to get: new keys
+                #opentok_ses = OpenTok(TEST_API_KEY, TEST_API_SECRET)
+                #session = opentok_ses.create_session(media_mode=MediaModes.relayed)
+                responce['apiKey'] = TEST_API_KEY
+                responce['sessionId'] = "test session id"#session.session_id
+                responce['token'] = "test token"#session.generate_token()
+                new_ses_keys = SessionKeys()
+                new_ses_keys.api_key = responce['apiKey']
+                new_ses_keys.session_id = responce['sessionId']
+                new_ses_keys.token = responce['token']
+                new_ses_keys.save()
+                new_con = Connection()
+                new_con.session_key = new_ses_keys
+                new_con.connections = 1
+                new_con.opener_app_key = app_key
+                new_con.save()
         return JsonResponse(responce)
     else:
         return None
